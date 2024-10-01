eee3c1e04f28104129043c44c37304d3ba67cd56
Raphael Schmid <raphaeljschmid@protonmail.com>
2024-10-01 12:33:50 -0600

    Finished CSV parsing in UI

--- ui/composables/csv.ts
+++ ui/composables/csv.ts
@@ -1,6 +1,17 @@
 import Papa from 'papaparse'
 
 export const useCSVHandler = (file: File): void => {
+  const requiredHeaders = [
+    'transaction_id',
+    'end_to_end_id',
+    'instd_amt',
+    'instd_amt_currency',
+    'xchg_rate_inf_unit_ccy',
+    'xchg_rate_inf_xchg_rate',
+    'cdtr_agt_bicfi',
+    'cdtr_acct_iban'
+  ]
+
   if (file && file instanceof Blob) {
     const reader = new FileReader()
 
@@ -8,9 +19,29 @@ export const useCSVHandler = (file: File): void => {
       const content = event.target?.result as string
 
       Papa.parse(content, {
-        header: true,
+        header: true, 
+        skipEmptyLines: true, 
         complete: (results) => {
-          console.log('Parsed CSV Data:', results.data)
+          const parsedData = results.data as Array<Record<string, string>>
+          const headers = results.meta.fields
+
+          if (!headers) {
+            console.error('Failed to retrieve headers from the CSV')
+            return
+          }
+
+          const missingHeaders = requiredHeaders.filter(header => !headers.includes(header))
+
+          if (missingHeaders.length > 0) {
+            console.error('Missing required headers:', missingHeaders)
+            return
+          }
+
+          const filteredData = parsedData.filter(row => {
+            return Object.values(row).some(value => value !== null && value !== '');
+          })
+
+          console.log('Array of CSV Data Objects:', filteredData)
         },
         error: (error: Error) => {
           console.error('Error parsing CSV:', error)
