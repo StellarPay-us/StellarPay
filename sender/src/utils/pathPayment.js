6a9c978417f78cbeb68fec16b26c0974a36ad955
Raphael Schmid <raphaeljschmid@protonmail.com>
2024-10-08 08:30:32 -0600

    Issue: Sender: Path Payment - Execute Pathpayment if exchange rate is in acceptable range

--- sender/src/utils/pathPayment.js
+++ sender/src/utils/pathPayment.js
@@ -1,60 +1,135 @@
+const StellarSdk = require("@stellar/stellar-sdk");
+
 /**
  * Get exchange prices, costs, and exchange rates for a path payment between two assets.
  *
- * @param {StellarSdk.Asset} sourceAsset - The asset being sent (from).
+ * @param {{code: string, issuer: string}} sourceInfo - The asset being sent (from).
  * @param {string} sourceAmount - The amount being sent.
- * @param {StellarSdk.Asset} destinationAsset - The asset to be received (to).
- * @param {StellarSdk.Server} server - The Stellar server instance to use.
+ * @param {{code: string, issuer: string}} destinationInfo - The asset to be received (to).
+ * @param {string} serverUrl - The Stellar server instance to use.
  * @returns {Promise<Array>} - Array of possible paths, exchange rates, and associated costs.
  */
-async function getExchangePricesAndCostsForPathPayment(
-  sourceAsset,
+const getExchangePricesAndCostsForPathPayment = async (
+  sourceInfo,
   sourceAmount,
-  destinationAsset,
-  server,
-) {
-  try {
-    const paths = await server
-      .strictSendPaths(sourceAsset, sourceAmount, [destinationAsset])
-      .call();
+  destinationInfo,
+  serverUrl,
+) => {
+  const sourceAsset = buildAsset(sourceInfo);
+  const destinationAsset = buildAsset(destinationInfo);
+  const server = setupServerConnection(serverUrl);
+  const paths = await server
+    .strictSendPaths(sourceAsset, sourceAmount, [destinationAsset])
+    .call();
 
-    const baseFee = await server.fetchBaseFee();
+  const baseFee = await server.fetchBaseFee();
 
-    const exchangePaths = paths.records.map(record => {
-      // Total number of hops in the path
-      const hops = record.path.length + 1; // +1 for the final destination asset
+  const exchangePaths = paths.records.map(record => {
+    // Total number of hops in the path
+    const hops = record.path.length + 1; // +1 for the final destination asset
 
-      // Transaction cost estimation: baseFee multiplied by the number of hops (one operation per hop)
-      const transactionCost = (baseFee * hops) / 10000000; // Convert stroops to XLM
+    // Transaction cost estimation: baseFee multiplied by the number of hops (one operation per hop)
+    const transactionCost = (baseFee * hops) / 10000000; // Convert stroops to XLM
 
-      // Calculate slippage (difference between source amount and final destination amount)
-      const slippage = (record.source_amount - sourceAmount) / sourceAmount;
+    // Calculate slippage (difference between source amount and final destination amount)
+    const slippage = (record.source_amount - sourceAmount) / sourceAmount;
 
-      // Calculate the exchange rate (destination amount per unit of source amount)
-      const exchangeRate = record.destination_amount / record.source_amount;
+    // Calculate the exchange rate (destination amount per unit of source amount)
+    const exchangeRate = record.destination_amount / record.source_amount;
 
-      return {
-        path: record.path.map(asset => {
-          if (!asset.asset_code || !asset.asset_issuer) {
-            return "XLM";
-          }
-          return `${asset.asset_code}:${asset.asset_issuer}`;
-        }),
-        sourceAmount: record.source_amount,
-        destinationAmount: record.destination_amount,
-        transactionCost: transactionCost.toFixed(7), // Cost in XLM for the transaction
-        slippage: slippage.toFixed(4), // Percentage slippage for the path
-        exchangeRate: exchangeRate.toFixed(7), // Exchange rate (destination per source)
-      };
-    });
+    return {
+      path: record.path.map(asset => {
+        if (!asset.asset_code || !asset.asset_issuer) {
+          return "XLM";
+        }
+        return `${asset.asset_code}:${asset.asset_issuer}`;
+      }),
+      sourceAmount: record.source_amount,
+      destinationAmount: record.destination_amount,
+      transactionCost: transactionCost.toFixed(7), // Cost in XLM for the transaction
+      slippage: slippage.toFixed(4), // Percentage slippage for the path
+      exchangeRate: exchangeRate.toFixed(7), // Exchange rate (destination per source)
+    };
+  });
 
-    return exchangePaths;
-  } catch (error) {
-    console.error("Error fetching exchange paths:", error);
-    throw error;
+  return exchangePaths;
+};
+
+/**
+ * Build the transaction to exchange and accept the conversion from the source asset to the destination asset.
+ *
+ * @param {StellarSdk.Keypair} keypair - The keypair of the wallet to exchange the assets
+ * @param {{code: string, issuer: string}} sourceInfo - The asset being sent (from).
+ * @param {string} sourceAmount - The amount being sent.
+ * @param {{code: string, issuer: string}} destinationInfo - The asset to be received (to).
+ * @param {string} destinationAmount - The minimum amount to be received.
+ * @param {string} serverUrl - The Stellar server instance to use.
+ * @returns {Promise<StellarSdk.Transaction<StellarSdk.Memo<StellarSdk.MemoType>>} - Transaction to accept and exchange from the source asset to the destination asset.
+ */
+
+const buildTransaction = async (
+  keypair,
+  sourceInfo,
+  sourceAmount,
+  destinationInfo,
+  destinationAmount,
+  serverUrl,
+) => {
+  const server = setupServerConnection(serverUrl);
+  const sourceAsset = buildAsset(sourceInfo);
+  const destinationAsset = buildAsset(destinationInfo);
+  const account = await server.loadAccount(keypair.publicKey());
+  const transaction = new StellarSdk.TransactionBuilder(account, {
+    fee: StellarSdk.BASE_FEE,
+    networkPassphrase: StellarSdk.Networks.TESTNET,
+  })
+    .addOperation(
+      StellarSdk.Operation.changeTrust({
+        asset: destinationAsset,
+      }),
+    )
+    .addOperation(
+      StellarSdk.Operation.pathPaymentStrictSend({
+        sendAsset: sourceAsset,
+        sendAmount: sourceAmount,
+        destination: keypair.publicKey(),
+        destAsset: destinationAsset,
+        destMin: destinationAmount,
+      }),
+    )
+    .setTimeout(30)
+    .build();
+
+  return transaction;
+};
+
+/**
+ * Sign and submit (execute) a built transaction
+ *
+ * @param {StellarSdk.Keypair} keypair - The keypair of the wallet
+ * @param {string} serverUrl - The Stellar server instance to use.
+ */
+const signAndSubmitTx = async (transaction, keypair, serverUrl) => {
+  const server = setupServerConnection(serverUrl);
+  transaction.sign(keypair);
+  await server.submitTransaction(transaction);
+};
+
+// Helper functions
+const buildAsset = assetInfo => {
+  if (assetInfo.code === "native") {
+    return StellarSdk.Asset.native();
+  } else {
+    return new StellarSdk.Asset(assetInfo.code, assetInfo.issuer);
   }
-}
+};
+
+const setupServerConnection = serverUrl => {
+  return new StellarSdk.Horizon.Server(serverUrl);
+};
 
 module.exports = {
   getExchangePricesAndCostsForPathPayment,
+  buildTransaction,
+  signAndSubmitTx,
 };
--- sender/tests/wallet.test.js
+++ sender/tests/wallet.test.js
@@ -1,16 +1,40 @@
-const StellarSdk = require('@stellar/stellar-sdk');
-const { getExchangePricesAndCostsForPathPayment } = require('../src/utils/pathPayment'); 
+const StellarSdk = require("@stellar/stellar-sdk");
+const { getExchangePricesAndCostsForPathPayment, buildTransaction, signAndSubmitTx } = require('../src/utils/pathPayment'); 
 
-const server = new StellarSdk.Horizon.Server('https://horizon-testnet.stellar.org');
+const getAssetBalance = (account, asset) => {
+  if (asset.isNative()) {
+      // Check for XLM (native asset)
+      const xlmBalance = account.balances.find(balance => balance.asset_type === 'native');
+      return xlmBalance ? xlmBalance.balance : '0';
+  } else {
+      // Check for custom assets
+      const assetBalance = account.balances.find(balance => 
+          balance.asset_code === asset.getCode() && balance.asset_issuer === asset.getIssuer()
+      );
+      return assetBalance ? assetBalance.balance : '0';
+  }
+};
 
 describe("Test Sender Wallet", () => {
-  it("should fetch exchange prices and costs correctly for valid assets", async () => {
-    // Define source and destination assets
-    const sourceAsset = StellarSdk.Asset.native(); // XLM (Native)
-    const destinationAsset = new StellarSdk.Asset('USDC', 'GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5'); // USD asset on the testnet
+  let serverUrl, sourceInfo, destinationInfo, keypair;
 
+  beforeAll(async () => {
+    serverUrl = 'https://horizon-testnet.stellar.org';
+    sourceInfo = {
+      code: 'native',
+      issuer: ''
+    };
+    destinationInfo = {
+      code: 'USDC',
+      issuer: 'GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5'
+    };
+
+    keypair = StellarSdk.Keypair.fromSecret('SBNZI3XQBQWPEKKAO2TRGJMBFUDUONJL4W3UK6EHIXINVIRHCFYIABUQ'); 
+  }); 
+
+  it("should fetch exchange prices and costs correctly for valid assets", async () => {
     try {
-      const result = await getExchangePricesAndCostsForPathPayment(sourceAsset, '10', destinationAsset, server);
+      const result = await getExchangePricesAndCostsForPathPayment(sourceInfo, '10', destinationInfo, serverUrl);
 
       expect(Array.isArray(result)).toBe(true);
 
@@ -28,19 +52,82 @@ describe("Test Sender Wallet", () => {
       console.error(error);
       throw new Error("Failed to fetch exchange prices and costs from the Stellar testnet.");
     }
-  });
+  }, 12000);
 
   it("should handle network issues or timeouts gracefully", async () => {
-    const sourceAsset = StellarSdk.Asset.native(); // XLM (Native)
-    const destinationAsset = new StellarSdk.Asset('USDC', 'GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5');
-
     try {
-      const invalidServer = new StellarSdk.Server('https://invalid-server-url');
-
-      await getExchangePricesAndCostsForPathPayment(sourceAsset, '10', destinationAsset, invalidServer);
+      await getExchangePricesAndCostsForPathPayment(sourceInfo, '10', destinationInfo, 'https://invalid-server-url');
 
     } catch (error) {
-      expect(error.message).toMatch(/StellarSdk.Server is not a constructor/);
+      expect(error.message).toMatch(/getaddrinfo ENOTFOUND invalid-server-url/);
     }
   });
+
+  it("should build a transaction to exchange assets", async () => {
+    const sourceAmount = '10'; 
+    const destinationAmount = '9.5'; 
+  
+    const transaction = await buildTransaction(
+      keypair,
+      sourceInfo,
+      sourceAmount,
+      destinationInfo,
+      destinationAmount,
+      serverUrl
+    );
+
+    // Ensure the transaction is built correctly
+    expect(transaction).toBeInstanceOf(StellarSdk.Transaction);
+    expect(transaction.operations.length).toBe(2);
+  
+    // Check first operation is a changeTrust for the destination asset
+    const changeTrustOperation = transaction.operations[0];
+    expect(changeTrustOperation.type).toBe('changeTrust');
+    expect(changeTrustOperation.line.code).toBe(destinationInfo.code);
+    expect(changeTrustOperation.line.issuer).toBe(destinationInfo.issuer);
+  
+    // Check second operation is a pathPaymentStrictSend for the source asset to destination asset
+    const pathPaymentOperation = transaction.operations[1];
+    expect(pathPaymentOperation.type).toBe('pathPaymentStrictSend');
+    expect(pathPaymentOperation.sendAmount).toBe('10.0000000');
+    expect(pathPaymentOperation.destMin).toBe('9.5000000');
+  
+    // Ensure the transaction has a timeout and a network fee
+    expect(transaction.timeBounds.maxTime).not.toBe('0'); // Timeout should be set
+  }, 12000);
+
+  it('should execute a transaction and update the wallet balance', async () => {
+    const server = new StellarSdk.Horizon.Server(serverUrl);
+    const sourceAsset = StellarSdk.Asset.native();
+    const destinationAsset =  new StellarSdk.Asset(destinationInfo.code, destinationInfo.issuer);
+
+    const senderAccount = await server.loadAccount(keypair.publicKey());
+    
+    const initalBalanceXLM = getAssetBalance(senderAccount, sourceAsset);
+    const initalBalanceUSDC = getAssetBalance(senderAccount, destinationAsset);
+
+    const rate = await getExchangePricesAndCostsForPathPayment(sourceInfo, '10', destinationInfo, serverUrl);
+    const transaction = await buildTransaction(
+      keypair,
+      sourceInfo,
+      '10',
+      destinationInfo,
+      rate[0].destinationAmount,
+      serverUrl
+    );
+    await signAndSubmitTx(transaction, keypair, serverUrl);
+
+    const updatedSenderAccount = await server.loadAccount(keypair.publicKey());
+    const finalBalanceXLM = getAssetBalance(updatedSenderAccount, sourceAsset);
+    const finalBalanceUSDC = getAssetBalance(updatedSenderAccount, destinationAsset);
+
+    // Round both values to 7 decimal places to avoid precision issues
+    const roundedFinalBalanceXLM = Number(finalBalanceXLM).toFixed(1);
+    const roundedExpectedBalanceXLM = (initalBalanceXLM - 10).toFixed(1);
+    const roundedFinalBalanceUSDC = Number(finalBalanceUSDC).toFixed(1);
+    const roundedExpectedBalanceUSDC = (Number(initalBalanceUSDC) + Number(rate[0].destinationAmount)).toFixed(1);
+
+    expect(roundedFinalBalanceXLM).toBe(roundedExpectedBalanceXLM);
+    expect(roundedFinalBalanceUSDC).toBe(roundedExpectedBalanceUSDC);
+  }, 30000);
 });
