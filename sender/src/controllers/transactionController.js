7d1a1349eb2af2e02d8ed05f7c405ddab776d281
Raphael Schmid <raphaeljschmid@protonmail.com>
2024-10-10 14:59:52 -0600

    Issue: Sender: Integrate the Path Payment into the Sender flow

--- sender/src/controllers/transactionController.js
+++ sender/src/controllers/transactionController.js
@@ -1,12 +1,71 @@
 const { parseTransaction } = require("../utils/sep31Parser");
+const { getExchangePricesAndCostsForPathPayment, buildTransaction, signAndSubmitTx } = require("../utils/pathPayment");
+const StellarSdk = require("@stellar/stellar-sdk");
+const axios = require("axios");
 
 exports.receiveTransaction = async (req, res) => {
 
   try {
-    // Parse the received message
-    const parsedResult = parseTransaction(req.body.message);
+    const keypair = StellarSdk.Keypair.random();
+    const publicKey = pair.publicKey();
+    const secretKey = pair.secret();
 
-    console.log(parsedResult)
+    console.log("Public Key:", publicKey);
+    console.log("Secret Key:", secretKey);
+
+    const friendbotUrl = `https://friendbot.stellar.org?addr=${publicKey}`;
+
+    console.log("Requesting testnet lumens...");
+    const response = await axios.get(friendbotUrl);
+
+    if (response.status === 200) {
+      console.log("Testnet lumens successfully funded to the wallet.");
+      console.log("Wallet setup complete.");
+    } else {
+      console.error("Failed to fund testnet lumens.");
+    }
+
+    const message = req.body.message;
+
+    // Setting up assets
+    const sourceAsset = {
+      code: 'native',
+      issuer: ''
+    };
+    const serverUrl = 'https://horizon-testnet.stellar.org';
+
+    for(const transaction of message.transactions) {
+      const destinationAsset = {
+        code: message.sender.currency,
+        issuer: 'GBBD47IF6LWK7P7MDEVSCWR7DPUWV3NY3DTQEVFL4NAT4AQH3ZLLFLA5'
+      };
+      // Check path payment rate
+      const rate = await getExchangePricesAndCostsForPathPayment(sourceAsset, transaction.amount.toString(), destinationAsset, serverUrl);
+      
+      /**@TODO Find the exchange rate clostest to the expected exchange rate */
+      
+      // Build transaction
+      const tx = await buildTransaction(
+        keypair,
+        sourceAsset,
+        transaction.amount,
+        destinationAsset,
+        rate[0].destinationAmount,
+        serverUrl
+      );
+
+      // Sign and submit the transaction
+      await signAndSubmitTx(tx, keypair, serverUrl);
+
+      // Parse the received message
+      const parsedResult = parseTransaction(message);
+
+      /**@TODO Post parsedResult.disbursement to SDP */
+
+      /**@TODO Fund wallet on SDP with the destination assets */
+
+      /**@TODO Post parsedResult.csvFile to kickstart the disbursement */
+    }
 
     res.status(201).send(`Transaction received and processed: ${JSON.stringify(req.body)}`);
   } catch (error) {
--- sender/src/server.js
+++ sender/src/server.js
@@ -1,6 +1,4 @@
 const app = require("./app");
-const StellarSdk = require("@stellar/stellar-sdk");
-const axios = require("axios");
 
 const port = 3020;
 
@@ -8,29 +6,6 @@ let intervalId;
 
 const server = app.listen(port, async () => {
   console.log(`Server listening at http://localhost:${port}`);
-
-  try {
-    const pair = StellarSdk.Keypair.random();
-    const publicKey = pair.publicKey();
-    const secretKey = pair.secret();
-
-    console.log("Public Key:", publicKey);
-    console.log("Secret Key:", secretKey);
-
-    // const friendbotUrl = `https://friendbot.stellar.org?addr=${publicKey}`;
-
-    // console.log("Requesting testnet lumens...");
-    // const response = await axios.get(friendbotUrl);
-
-    // if (response.status === 200) {
-    //   console.log("Testnet lumens successfully funded to the wallet.");
-    //   console.log("Wallet setup complete.");
-    // } else {
-    //   console.error("Failed to fund testnet lumens.");
-    // }
-  } catch (error) {
-    console.error("Error setting up Stellar wallet:", error.message);
-  }
 });
 
 const closeServer = () => {
